05cf07f74bd7de50f8aec1d54a11f0658dbb55cc
Karthikeyan C <114332111+cyber-karthi@users.noreply.github.com>
2024-06-21 14:57:35 +0530

    Update const.py

--- laravelignitionRxss/utils/const.py
+++ laravelignitionRxss/utils/const.py
@@ -12,7 +12,7 @@
 
 
 class Data:
-    blog = 'https://blogs.cappriciosec.com/blog/137/laravel-ignition-Rxss'
+    blog = 'https://blogs.cappriciosec.com/blog/162/laravel-ignition-Rxss'
     api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
     config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
     payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/laravel-ignition-Rxss.txt'
